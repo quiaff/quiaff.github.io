634b22b29c4a9f8edb99a88f1bee7f4491cf2137
quiaff <43115370+quiaff@users.noreply.github.com>
2019-09-21 15:55:38 -0300

    Cambios cache service worker
    Para modo offline y borrar cache viejo

--- sw.js
+++ sw.js
@@ -1,34 +1,54 @@
 
-const cacheActual = 'UAIMobile-v2';
+const cacheActual = 'Grupo2-v1';
 
 const recursosEstaticos = [
-  'css/materialize.min.css',  
-  'js/materialize.min.js' , 
+  'css/materialize.min.css',
+  'css/estilos.css',
+  'js/materialize.min.js',
   'icons/512.png',
-  'icons/192.png'   
+  'icons/192.png',
+  'img/gaby_chica.jpg',
+  'img/julian_chica.jpg',
+  'img/vanesa_chica.jpg',
+  'img/nayi_chica.jpg',
+  'index.html',
+  'tp1.html'
 ];
 
-self.addEventListener('install', function(event) 
-{
-  event.waitUntil
-  (
-    caches.open(cacheActual).then(function(cache) 
-       {
-        return cache.addAll(recursosEstaticos);
-       })
-  );
-});	 
-	  
+self.addEventListener('install', function (event) {
+    event.waitUntil
+    (
+      caches.open(cacheActual).then(function (cache) {
+          return cache.addAll(recursosEstaticos);
+      })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    var version = 'v1';
+    event.waitUntil(
+      caches.keys()
+        .then(cacheNames =>
+            Promise.all(
+              cacheNames
+                .map(c => c.split('-'))
+                .filter(c => c[0] === 'Grupo2')
+                .filter(c => c[1] !== version)
+                .map(c => caches.delete(c.join('-')))
+            )
+        )
+    );
+});
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
 });
 
