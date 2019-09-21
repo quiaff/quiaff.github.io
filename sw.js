fa1a3a2f15059fa6ee89406c33619e680e156953
quiaff <43115370+quiaff@users.noreply.github.com>
2019-09-21 16:01:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,6 +24,21 @@ self.addEventListener('install', function (event) {
     );
 });
 
+self.addEventListener('activate', function (event) {
+    var version = 'v1';
+    event.waitUntil(
+      caches.keys()
+        .then(cacheNames =>
+            Promise.all(
+              cacheNames
+                .map(c => c.split('-'))
+                .filter(c => c[0] === 'Grupo2')
+                .filter(c => c[1] !== version)
+                .map(c => caches.delete(c.join('-')))
+            )
+        )
+    );
+});
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
