19cc082f35c465cdc4828a02cc85d13d931bddfe
quiaff <43115370+quiaff@users.noreply.github.com>
2019-09-26 18:25:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -26,6 +26,23 @@ self.addEventListener('install', function (event) {
     );
 });
 
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+          return Promise.all(keyList.map(function(key) {
+            if (key !== cacheName) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }));
+        })
+    );
+
+    return self.clients.claim();
+});
+
 self.addEventListener('fetch', function (event) {
     event.respondWith(
       caches.match(event.request)
